41b2aae8f0be7a9294219ee1ed352a95c26dbf0d
Nagoor049 <mail2nagoor22@gmail.com>
2024-03-08 09:22:30 +0000

    solution

---
+++ src/main/resources/data.sql
@@ -0,0 +1,26 @@
+Insert INTO product(productName, price) 
+VALUES('Smartphone', 599.99);
+
+Insert INTO product(productName, price) 
+VALUES('Laptop', 1299.99);
+
+Insert INTO product(productName, price)
+VALUES('Gaming Console', 399.99); 
+
+Insert INTO review(reviewContent, rating, productId) 
+VALUES('Great battery life!', 5, 1);
+
+Insert INTO review(reviewContent, rating, productId) 
+VALUES('Lags sometimes.', 3, 1); 
+
+Insert INTO review(reviewContent, rating, productId) 
+VALUES('Perfect for my daily tasks!', 4, 2); 
+
+Insert INTO review(reviewContent, rating, productId) 
+VALUES('Bit pricey, but worth it.', 4, 2);
+
+Insert INTO review(reviewContent, rating, productId) 
+VALUES('Awesome gaming experience!', 5, 3);
+
+Insert INTO review(reviewContent, rating, productId) 
+VALUES('Needs more exclusive games.', 4, 3);
